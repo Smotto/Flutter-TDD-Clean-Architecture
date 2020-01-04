1d86cf6e089edea75c6528d35a14ee338357fdc6
Jarod Castillo <Jarodc2017@gmail.com>
2020-01-03 17:19:46 -0800

    fixed path

--- test/fixtures/fixture_reader.dart
+++ test/fixtures/fixture_reader.dart
@@ -1,5 +1,4 @@
 import 'dart:io';
 
-String fixture(String name) => File(
-        'C:/Users/Lazyf/Documents/CodingProjects/FlutterApps/clean_architecture_tdd_course/test/fixtures/$name')
-    .readAsStringSync();
+String fixture(String name) =>
+    File('../test/fixtures/$name').readAsStringSync();
