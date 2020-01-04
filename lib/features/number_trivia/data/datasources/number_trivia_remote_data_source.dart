5f316baba5eef5b9652783093a06f09c4d4a9484
Jarod Castillo <Jarodc2017@gmail.com>
2020-01-04 00:01:57 -0800

    Bloc Scaffolding & Input Conversion

--- lib/features/number_trivia/data/datasources/number_trivia_remote_data_source.dart
+++ lib/features/number_trivia/data/datasources/number_trivia_remote_data_source.dart
@@ -1,13 +1,49 @@
-import 'package:clean_architecture_tdd_course/features/number_trivia/domain/entities/number_trivia.dart';
+import 'dart:convert';
+
+import 'package:clean_architecture_tdd_course/core/error/exceptions.dart';
+import 'package:clean_architecture_tdd_course/features/number_trivia/data/models/number_trivia_model.dart';
+import 'package:http/http.dart' as http;
+import 'package:meta/meta.dart';
+
+import '../../domain/entities/number_trivia.dart';
 
 abstract class NumberTriviaRemoteDataSource {
   /// Calls the http://numbers.api.com/${number} endpoint.
   ///
   /// Throws a [ServerException] for all error codes.
-  Future<NumberTrivia> getConcreteNumberTrivia(int number);
+  Future<NumberTriviaModel> getConcreteNumberTrivia(int number);
 
   /// Calls the http://numbers.api.com/random endpoint.
   ///
   /// Throws a [ServerException] for all error codes.
-  Future<NumberTrivia> getRandomNumberTrivia();
+  Future<NumberTriviaModel> getRandomNumberTrivia();
+}
+
+class NumberTriviaRemoteDataSourceImpl implements NumberTriviaRemoteDataSource {
+  final http.Client client;
+
+  NumberTriviaRemoteDataSourceImpl({@required this.client});
+
+  @override
+  Future<NumberTriviaModel> getConcreteNumberTrivia(int number) =>
+      _getTriviaFromUrl('http://numbersapi.com/$number');
+
+  @override
+  Future<NumberTriviaModel> getRandomNumberTrivia() =>
+      _getTriviaFromUrl('http://numbersapi.com/random');
+
+  Future<NumberTriviaModel> _getTriviaFromUrl(String url) async {
+    final response = await client.get(
+      url,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    );
+
+    if (response.statusCode == 200) {
+      return NumberTriviaModel.fromJson(json.decode(response.body));
+    } else {
+      throw ServerException();
+    }
+  }
 }
