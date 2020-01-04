eda72dc4b7d30e4c386b0347ebdb5ada92367c48
Jarod Castillo <Jarodc2017@gmail.com>
2020-01-04 00:54:14 -0800

    should call the InputConverter to validate and convert the string to an unsigned integer

--- lib/features/number_trivia/presentation/bloc/number_trivia_bloc.dart
+++ lib/features/number_trivia/presentation/bloc/number_trivia_bloc.dart
@@ -1,8 +1,31 @@
 import 'dart:async';
 import 'package:bloc/bloc.dart';
+import 'package:clean_architecture_tdd_course/core/util/input_converter.dart';
+import 'package:clean_architecture_tdd_course/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart';
+import 'package:clean_architecture_tdd_course/features/number_trivia/domain/usecases/get_random_number_trivia.dart';
 import './bloc.dart';
+import 'package:meta/meta.dart';
+
+const String SERVER_FAILURE_MESSAGE = 'Server Failure';
+const String CACHE_FAILURE_MESSAGE = 'Cache Failure';
+const String INVALID_INPUT_FAILURE_MESSAGE =
+    'Invalid Input - The number must be positive integer or zero.';
 
 class NumberTriviaBloc extends Bloc<NumberTriviaEvent, NumberTriviaState> {
+  final GetConcreteNumberTrivia getConcreteNumberTrivia;
+  final GetRandomNumberTrivia getRandomNumberTrivia;
+  final InputConverter inputConverter;
+
+  NumberTriviaBloc(
+      {@required GetConcreteNumberTrivia concrete,
+      @required GetRandomNumberTrivia random,
+      @required this.inputConverter})
+      : assert(concrete != null),
+        assert(random != null),
+        assert(inputConverter != null),
+        getConcreteNumberTrivia = concrete,
+        getRandomNumberTrivia = random;
+
   @override
   NumberTriviaState get initialState => Empty();
 
@@ -11,5 +34,9 @@ class NumberTriviaBloc extends Bloc<NumberTriviaEvent, NumberTriviaState> {
     NumberTriviaEvent event,
   ) async* {
     // TODO: Add Logic
+    if (event is GetTriviaForConcreteNumber) {
+      inputConverter.stringToUnsignedInteger(event.numberString);
+      yield Error(message: 'Invalid Input');
+    }
   }
 }
